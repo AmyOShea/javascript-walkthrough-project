fab09bdd25a5a7962c4d23538c51aad0037a2101
AmyOShea <amyoshea.lsad@gmail.com>
2021-01-25 17:09:42 +0000

    Multiplying to make divisibles

--- script.js
+++ script.js
@@ -34,6 +34,20 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    let a = Math.floor(Math.random() * 12) + 1;
+    let b = Math.floor(Math.random() * 12) + 1;
+    
+    let numD1 = (a * b);
+    
+    let numD2 = b;
+
+
+    //let divNum1 = num1 * 10;
+
+   // do { divNum2--
+   // } while (num1 % divNum !== 0);
+
+
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
     } 
@@ -42,6 +56,9 @@ function runGame(gameType) {
     } 
     else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2);
+    }
+    else if (gameType === 'division') {
+        displayDivisionQuestion(numD1, numD2);
     } 
     else {
         alert(`Unknown game type ${gameType}`);
@@ -84,6 +101,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, 'multiply'];
     } else if (operator === '-'){
         return [operand1 - operand2, 'subtract'];
+    } else if (operator === '/'){
+        return [operand1 / operand2, 'division'];
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, aborting!`;
@@ -129,4 +148,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '/';
+
 }
